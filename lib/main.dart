a60cc55ea424e3e5284d6a2fce4f1f504ca9de12
Cristiano ramos <cristianorag@gmail.com>
2021-11-15 15:46:09 -0300

    Iniciando Projeto My Card

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MeuApp(),
+  );
+}
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        backgroundColor: Colors.deepOrange,
+        body: SafeArea(
+          child: Container(
+            padding: EdgeInsets.all(20.0),
+            margin: EdgeInsets.only(left: 30.0),
+            height: 100.0,
+            width: 100.0,
+            color: Colors.white,
+            child: Text('Hello Word!'),
+          )
+        ),
+      ),
+    );
+  }
+}
+
